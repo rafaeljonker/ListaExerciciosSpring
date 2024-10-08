9a44d50d47d74f134a23878ea865ffa043f9a2c6
rafaeljonker <152179966+rafaeljonker@users.noreply.github.com>
2024-10-08 14:24:31 -0300

    RIANDOJAVASHOW

--- 5-JWTSecurityNivelB/SecurityWeb/src/main/java/com/example/SecurityWeb/controller/LoginController.java
+++ 5-JWTSecurityNivelB/SecurityWeb/src/main/java/com/example/SecurityWeb/controller/LoginController.java
@@ -6,10 +6,7 @@ import com.example.SecurityWeb.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
@@ -25,4 +22,9 @@ public ResponseEntity<String> registration(@Valid @RequestBody UserRegistrationD
     return new ResponseEntity<>("Usuario cadastrado com sucesso", HttpStatus.CREATED);
 }
 
+    @GetMapping("teste")
+    public String registrationa() {
+       // userService.registration(userRegistrationDto);
+        return "Vc esta logado";
+    }
 }
